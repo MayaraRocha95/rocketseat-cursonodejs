796830e85613b2333f46ae7c9314b82dafa6691b
Mayara <mayaraaraujorocha@hotmail.com>
2023-11-10 11:52:54 -0300

    aula 2 fundamentos

--- src/server.js
+++ src/server.js
@@ -1,11 +1,27 @@
-import  http from "node:http";
+import http from "node:http";
 
+const users = [];
 
 const server = http.createServer((req, res) => {
   const  { method, url } = req
-  
-  console.log(method, url )
-  return res.end('Hello Word') 
-})
+ 
+    if (method === 'GET' && url === '/users') {
+        
+        return res
+        .setHeader('Content-type' , 'application/json')
+        .end(JSON.stringify(users));
+    }
+    if (method === 'POST' && url === '/users') {
+        users.push({
+            id: 1, 
+            nome: 'Mango Joe',
+            email: 'mangojoe@tiktok.com',
+        });
 
-server.listen(3333)
+        return res.writeHead(201).end();
+    }
+
+    return res.writeHead(201).end('Not Found'); 
+});
+
+server.listen(3333);
