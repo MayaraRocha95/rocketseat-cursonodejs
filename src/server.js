f75d007c360206516fef49479215895675bb0736
Mayara <mayaraaraujorocha@hotmail.com>
2023-11-17 11:04:41 -0300

    copo de requisição de JSON

--- src/server.js
+++ src/server.js
@@ -2,26 +2,40 @@ import http from "node:http";
 
 const users = [];
 
-const server = http.createServer((req, res) => {
-  const  { method, url } = req
- 
-    if (method === 'GET' && url === '/users') {
-        
-        return res
-        .setHeader('Content-type' , 'application/json')
-        .end(JSON.stringify(users));
-    }
-    if (method === 'POST' && url === '/users') {
-        users.push({
-            id: 1, 
-            nome: 'Mango Joe',
-            email: 'mangojoe@tiktok.com',
-        });
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
 
-        return res.writeHead(201).end();
-    }
+  const buffers = [];
 
-    return res.writeHead(201).end('Not Found'); 
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch {
+    req.body = null;
+  }
+
+  //   console.log(body)
+
+  if (method === "GET" && url === "/users") {
+    return res
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users));
+  }
+  if (method === "POST" && url === "/users") {
+    const { name, email } = req.body;
+    users.push({
+      id: 1,
+      name,
+      email,
+    });
+
+    return res.writeHead(201).end();
+  }
+
+  return res.writeHead(201).end("Not Found");
 });
 
 server.listen(3333);
